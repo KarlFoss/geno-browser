a1cc49f9759097272f2fd24790bd3649ecd457a9
Kyle <mbio.kyle@gmail.com>
2015-03-14 15:34:47 -0500

    Implemented GET route for user

--- angular_flask/controllers.py
+++ angular_flask/controllers.py
@@ -1,16 +1,16 @@
 import os
 
 from flask import Flask, request, Response
-from flask import render_template, url_for, redirect, send_from_directory
+from flask import render_template, url_for, redirect, send_from_directory, jsonify
 from flask import send_file, make_response, abort
 
 from angular_flask import parse_data
 
 from angular_flask import app
+from angular_flask import db
 
 # routing for API endpoints (generated from the models designated as API_MODELS)
 from angular_flask.models import *
-
 
 # routing for basic pages (pass routing onto the Angular app)
 @app.route('/')
@@ -27,24 +27,27 @@ def gene():
         f = request.files['file']
         return str(parse_data.parse_file(f).shape)
 
-# routing for CRUD-style endpoints
-# passes routing onto the angular frontend if the requested resource exists
-from sqlalchemy.sql import exists
-
-crud_url_models = app.config['CRUD_URL_MODELS']
-
-@app.route('/<model_name>/')
-@app.route('/<model_name>/<item_id>')
-def rest_pages(model_name, item_id=None):
-	if model_name in crud_url_models:
-		model_class = crud_url_models[model_name]
-		if item_id is None or session.query(exists().where(
-			model_class.id == item_id)).scalar():
-			return make_response(open(
-				'angular_flask/templates/index.html').read())
-	abort(404)
-
-# special file handlers and error handlers
+#@app.route('/<model_name>/')
+#@app.route('/<model_name>/<item_id>')
+@app.route('/users/<user_id>',methods=['GET','POST','PUT','DELETE'])
+def user(user_id):
+    if request.method == 'GET':
+        user = db.session.query(User).get(user_id)
+        if(user):
+            return jsonify( user_name=user.user_name, user_id=user.id, email=user.email)
+        else:
+            return jsonify(message="Can't fetch user",code=1)
+#    elif request.method == 'POST':
+#        json = request.get_json()
+#        print json
+#        return json
+#    elif request.method == 'PUT':
+#    	
+#    elif request.method == 'DELETE':
+#    	#
+#    else
+#    	#
+## special file handlers and error handlers
 @app.route('/favicon.ico')
 def favicon():
 	return send_from_directory(os.path.join(app.root_path, 'static'),
