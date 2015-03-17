5ac692e3b8465ae8803f9b6b7484ca4281dbbd6f
Kyle <mbio.kyle@gmail.com>
2015-03-16 20:47:18 -0500

    Making progress on user api, looking for a good solution to json validation

--- angular_flask/controllers.py
+++ angular_flask/controllers.py
@@ -5,9 +5,7 @@ from flask import render_template, url_for, redirect, send_from_directory, jsoni
 from flask import send_file, make_response, abort
 
 from angular_flask import parse_data
-
-from angular_flask import app
-from angular_flask import db
+from angular_flask import app, db, session
 
 # routing for API endpoints (generated from the models designated as API_MODELS)
 from angular_flask.models import *
@@ -27,26 +25,56 @@ def gene():
         f = request.files['file']
         return str(parse_data.parse_file(f).shape)
 
-#@app.route('/<model_name>/')
-#@app.route('/<model_name>/<item_id>')
-@app.route('/users/<user_id>',methods=['GET','POST','PUT','DELETE'])
-def user(user_id):
-    if request.method == 'GET':
-        user = db.session.query(User).get(user_id)
-        if(user):
-            return jsonify( user_name=user.user_name, user_id=user.id, email=user.email)
-        else:
-            return jsonify(message="Can't fetch user with id: "+user_id),404
-#    elif request.method == 'POST':
-#        json = request.get_json()
-#        print json
-#        return json
-#    elif request.method == 'PUT':
-#    	
-#    elif request.method == 'DELETE':
-#    	#
-#    else
-#    	#
+@app.route('/users/',methods=['POST'])
+def new_user():
+    # Parse the json
+    json = request.get_json()
+    u_name = json.get('user_name')
+    email = json.get('email')
+    
+    # Ensure user_name and email were passed
+    if not u_name:
+        return jsonify(response="Could not create user, user_name field is required"),404
+
+    if not email:
+        return jsonify(response="Could not create user. email field is required"),404
+
+    # Crete user and commit 
+    new_user = User(user_name=json.get('user_name'),email=json.get('email'))
+    session.add(new_user)
+    session.commit()
+    
+    # Last check to make sure it was commited properly
+    if not new_user.id:
+        return jsonify(response="Could not create user"),404
+    
+    # Return the user id on success
+    return jsonify(user_id=new_user.id)
+
+@app.route('/users/<user_id>',methods=['GET'])
+def get_user(user_id):
+    user = session.query(User).get(user_id)
+    if(user):
+        return jsonify( user_name=user.user_name, user_id=user.id, email=user.email)
+    else:
+        return jsonify(response="Can't fetch user with id: "+user_id),404
+
+@app.route('/users/<user_id>',methods=['PUT'])
+def update_user(user_id):
+    json = request.get_json()
+    user = session.query(User).get(user_id)
+
+    # Set all fields
+    if json.get('user_name'):
+        user.user_name = json.get('user_name')
+
+    if json.get('email'):
+        user.email = json.get('email')
+
+    session.commit()
+    return jsonify(user_name=user.user_name)
+
+
 ## special file handlers and error handlers
 @app.route('/favicon.ico')
 def favicon():
