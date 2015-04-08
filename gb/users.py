2c1d4d0039d71aefadbac04c37177af9845efb64
Kyle <mbio.kyle@gmail.com>
2015-04-08 17:21:34 -0500

    updated users endpoints with password stuff

--- gb/users.py
+++ gb/users.py
@@ -3,20 +3,26 @@ from gb import app, db, session
 from gb.models import User
 from controllers import check_headers
 
-@app.route('/api/users/<int:user_id>',methods=['GET'])
-def get_user(user_id):
+@app.route('/api/users/',methods=['GET'])
+@check_headers
+def get_user():
+
+    user_id = request.user_id  
     user = session.query(User).get(user_id)
+
     if(user):
-        return jsonify( username=user.username, user_id=user.id, email=user.email)
+        return jsonify( username=user.username, user_id=user.id, email=user.email )
     else:
         return jsonify(response="Can't fetch user with id: {}".format(user_id)),404
 
 @app.route('/api/users',methods=['POST'])
 def new_user():
+
     # Parse the json
     json = request.get_json()
     u_name = json.get('username')
     email = json.get('email')
+    password = json.get('password')
     
     # Ensure username and email were passed
     if not u_name:
@@ -26,7 +32,7 @@ def new_user():
         return jsonify(response="Could not create user. email field is required"),404
 
     # Crete user and commit 
-    new_user = User(username=json.get('username'),email=json.get('email'))
+    new_user = User(username=json.get('username'),email=json.get('email'),password=json.get('password'))
     session.add(new_user)
     session.commit()
     
@@ -37,8 +43,11 @@ def new_user():
     # Return the user id on success
     return jsonify(user_id=new_user.id)
 
-@app.route('/api/users/<int:user_id>',methods=['PUT'])
-def update_user(user_id):
+@app.route('/api/users/',methods=['PUT'])
+@check_headers
+def update_user():
+
+    user_id = request.user_id
     json = request.get_json()
     user = session.query(User).get(user_id)
 
@@ -54,8 +63,11 @@ def update_user(user_id):
     session.commit()
     return jsonify(username=user.username, user_id=user.id, email=user.email)
 
-@app.route('/api/users/<int:user_id>',methods=['DELETE'])
-def delete_user(user_id):
+@app.route('/api/users/',methods=['DELETE'])
+@check_headers
+def delete_user():
+    
+    user_id = request.user_id
     user = session.query(User).get(user_id)
 
     if not user:
