5dfbd36bae8193486e0f4c23aff0b94e0c09c585
Max Eggers <eggers.max@gmail.com>
2015-04-08 08:45:12 -0500

    added file upload directive

--- gb/static/js/directives.js
+++ gb/static/js/directives.js
@@ -2,11 +2,29 @@
 'use strict';
 
 /* Directives */
-    angular.module('genoBrowserDirectives', ['genoBrowserControllers'])
-        .directive('gbWigPlot', function(){
-            return {
-                restrict:'E',
-                templateUrl:'partials/wig-plot.html',
-            };
-        });
+    var genoBrowserDirectives = angular.module('genoBrowserDirectives', ['genoBrowserControllers']);
+
+    genoBrowserDirectives.directive('gbWigPlot', function(){
+        return {
+            restrict:'E',
+            templateUrl:'partials/wig-plot.html',
+        };
+    });
+
+    genoBrowserDirectives.directive('fileModel', ['$parse', function ($parse) {
+        return {
+            restrict: 'A',
+            link: function(scope, element, attrs) {
+                var model = $parse(attrs.fileModel);
+                var modelSetter = model.assign;
+
+                element.bind('change', function(){
+                    scope.$apply(function(){
+                        modelSetter(scope, element[0].files[0]);
+                    });
+                });
+            }
+        };
+    }]);
+
 })();
