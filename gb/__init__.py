3df33c26f982940de2c1fe6d1d2a05b71f1e122a
Max Eggers <eggers.max@gmail.com>
2015-04-08 14:25:20 -0500

    added flask-httpauth

--- gb/__init__.py
+++ gb/__init__.py
@@ -2,6 +2,7 @@ import os
 import json
 from flask import Flask, request, Response
 from flask import render_template, send_from_directory, url_for
+from flask.ext.httpauth import HTTPBasicAuth
 from flask.ext.sqlalchemy import SQLAlchemy
 
 app = Flask(__name__)
@@ -9,6 +10,7 @@ app = Flask(__name__)
 app.config.from_object('gb.settings')
 db = SQLAlchemy(app)
 session = db.session
+auth = HTTPBasicAuth()
 app.url_map.strict_slashes = False
 
 import gb.models
