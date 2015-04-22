f0b4d0ca0149d1f8e123ac94b506c3472f96fbec
unclegeoff <ggilles93@gmail.com>
2015-04-22 14:18:44 -0500

    there's something super wrong

--- gb/static/js/directives.js
+++ gb/static/js/directives.js
@@ -4,10 +4,13 @@
 /* Directives */
     var genoBrowserDirectives = angular.module('genoBrowserDirectives', ['genoBrowserControllers','genoBrowserServices', 'ui.bootstrap', 'angularFileUpload']);
 
-    genoBrowserDirectives.directive('gbWigPlot', function(){
+    genoBrowserDirectives.directive('gbWigPlot', function($compile){
         return {
             restrict:'E',
-            templateUrl:'partials/wig-plot.html'
+            templateUrl:'partials/wig-plot.html',
+            link: function(scope, element, attrs){
+
+            }
         };
     });
 
@@ -112,6 +115,7 @@
                     $location.path('/view/' + view.view_id);
                     // Remember which view was loaded so we can quickly check
                     scope.loaded_view = view;
+
                 };
 
                 // This is called when the user clicks the add view button +
