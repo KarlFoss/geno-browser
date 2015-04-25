014baf7124e3cdb14ddff5d04547e07dd5fb0a3c
Max Eggers <eggers.max@gmail.com>
2015-04-25 12:09:16 -0500

    changed authentication to a jwt method and cleaned up imports

--- gb/__init__.py
+++ gb/__init__.py
@@ -1,9 +1,5 @@
-import os
-import json
-
 from flask import Flask, request, Response
-from flask import render_template, send_from_directory, url_for
-from flask.ext.httpauth import HTTPBasicAuth
+from flask_jwt import JWT
 from flask.ext.sqlalchemy import SQLAlchemy
 
 app = Flask(__name__, static_url_path="")
@@ -11,7 +7,7 @@ app = Flask(__name__, static_url_path="")
 app.config.from_object('gb.settings')
 db = SQLAlchemy(app)
 session = db.session
-auth = HTTPBasicAuth()
+jwt = JWT(app)
 app.url_map.strict_slashes = False
 
 import gb.models
--- gb/controllers.py
+++ gb/controllers.py
@@ -1,11 +1,19 @@
 import os
-from functools import wraps
 
 from flask import Flask, request, Response
 from flask import render_template, url_for, redirect, send_from_directory, jsonify
-from flask import send_file, make_response, abort, g
-from gb import app, auth, db, session, parse_data
+from gb import app, jwt, session
 from gb.models import *
+
+@jwt.authentication_handler
+def authenticate(username, password):
+    if username == 'joe' and password == 'pass':
+        return User(id=1, username='joe')
+
+@jwt.user_handler
+def load_user(payload):
+    if payload['user_id'] == 1:
+        return User(id=1, username='joe')
 
 # routing for basic pages (pass routing onto the Angular app)
 @app.route('/')
@@ -19,32 +27,3 @@ def favicon():
 @app.errorhandler(404)
 def page_not_found(e):
     return jsonify(response="Invalid Request"), 404
-
-@app.route('/api/token', methods=["GET"])
-@auth.login_required
-def get_token():
-    token = g.user.generate_token()
-    return jsonify({ 'X-Access-Token': token.decode('ascii') })
-
-@app.route('/gene_test',methods=['GET','POST'])
-def gene():
-    if request.method == 'GET':
-        return 'ADFS'
-    else:
-        f = request.files['file']
-        return str(parse_data.parse_file(f).shape)
-
-@auth.verify_password
-def verify_password(username_or_token, password):
-    # first try to authenticate by token
-    user = User.verify_token(username_or_token)
-    if not user:
-        # try to authenticate with username/password
-        user = User.query.filter_by(username = username_or_token).first()
-        if not user or not user.verify_password(password):
-            user = User.query.filter_by(username="default").first()
-
-    g.user = user
-    return True
-
-from gb import users,tracks,views,files
