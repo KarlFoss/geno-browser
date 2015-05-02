33bb5ed85245dcb087dbcc79af7915b321d55631
Max Eggers <eggers.max@gmail.com>
2015-05-02 18:17:41 -0500

    no functional change

--- gb/static/js/directives.js
+++ gb/static/js/directives.js
@@ -1,5 +1,5 @@
 (function(){
-'use strict';
+    'use strict';
 
     var genoBrowserDirectives = angular.module('genoBrowserDirectives', ['genoBrowserControllers','genoBrowserServices', 'ui.bootstrap', 'angularFileUpload']);
 
