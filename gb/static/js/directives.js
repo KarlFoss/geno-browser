65db4747655db80aff3c3703787b26aae9572a50
Max Eggers <eggers.max@gmail.com>
2015-05-02 18:08:39 -0500

    added are you sure? confirmation on delete of tracks/views

--- gb/static/js/directives.js
+++ gb/static/js/directives.js
@@ -3,6 +3,24 @@
 
     var genoBrowserDirectives = angular.module('genoBrowserDirectives', ['genoBrowserControllers','genoBrowserServices', 'ui.bootstrap', 'angularFileUpload']);
 
+    genoBrowserDirectives.directive('ngConfirmClick', [
+        function(){
+            return {
+                priority: -1,
+                restrict: 'A',
+                link: function(scope, element, attrs){
+                    element.bind('click', function(e){
+                        var message = attrs.ngConfirmClick;
+                        if(message && !confirm(message)){
+                            e.stopImmediatePropagation();
+                            e.preventDefault();
+                        }
+                    });
+                }
+            }
+        }
+    ]);
+
     genoBrowserDirectives.directive("resettableForm", ['$parse',
         function($parse) {
             return {
